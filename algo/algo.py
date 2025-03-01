39b1bf485fc0963c20c33c6dfe412a3c7bd63a92
Pratikala Karki <karkip5@uni.coventry.ac.uk>
2025-03-01 22:51:50 +0545

    Removed unnecessary folder

--- algo/algo.py
+++
@@ -1,6 +0,0 @@
-def term_n(n):
-  return ((((n**5 + n**3 + a*n**2 + a*n) * (n**7 + n**5 + a*n**3 + a*n**2 + 2*a*n + a)) % 100000 - 50000) // 1000 + 25) % 50 - 25
-
-a=1
-terms = [term_n(n) for n in range(1, 501)]
-print(terms)
